40574f478c4e6a66f66f64e1abd922875143db81
Greșanu Emanuel Vasile <emanuel2558@gmail.com>
2020-01-07 23:08:36 +0200

    initial commit

---
+++ src/main/java/com/tema/sda/Tema_SDA_3/TemaSda3Application.java
@@ -0,0 +1,13 @@
+package com.tema.sda.Tema_SDA_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemaSda3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemaSda3Application.class, args);
+	}
+
+}
