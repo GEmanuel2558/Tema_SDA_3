84ed36c7d3e90ca2db7a69d04f43606cbbe14c8a
Greșanu Emanuel Vasile <emanuel2558@gmail.com>
2020-01-11 15:02:24 +0200

    add the rest of the error handlers to the class RestExceptionHandler to handle the rest of the exceptions

--- src/main/java/com/tema/sda/Tema_SDA_3/error/handler/models/ApiValidationError.java
+++ src/main/java/com/tema/sda/Tema_SDA_3/error/handler/models/ApiValidationError.java
@@ -14,6 +14,13 @@ public class ApiValidationError extends ApiSubError {
         this.message = message;
     }
 
+    public ApiValidationError(String object, String field, Object rejectedValue, String message) {
+        this.object = object;
+        this.field = field;
+        this.rejectedValue = rejectedValue;
+        this.message = message;
+    }
+
     public String getObject() {
         return object;
     }
